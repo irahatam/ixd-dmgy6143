46ceb57cd92116b1d4d5ea50f89a7caf747fc37f
Matahari K Herwin <matahari@nyu.edu>
2021-10-26 11:33:52 -0400

    add array

--- bunchOHarries.js
+++ bunchOHarries.js
@@ -1,13 +1,20 @@
-let lot, lotharry;
+let lot, lotharry, beanieimg;
+var whichimage = 0;
+var theimages = [];
 
 function preload() {
   loadFont('ChampionGothic-Heavyweight.ttf');
+	theimages[0] = loadImage('lot.png');
+	theimages[1] = loadImage('beanie.png');
+	theimages[2] = loadImage('dunkirk.png');
+	theimages[3] = loadImage('fratboy.png');
+	theimages[4] = loadImage('harry.jpg');
+	theimages[5] = loadImage('2012.png');
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
 	background('#007FFF');
-	lotharry = loadImage('lot.png');
 	
 	lot = createButton('Love On Tour Harry');
 	lot.position(50,50);
@@ -18,11 +25,11 @@ function setup() {
 }
 
 function mousePressed() {
-	image(lotharry, mouseX, mouseY, 200, 200);
+	whichimage = (whichimage+1)%theimages.length;
 }
 
 function mouseDragged() {
-	image(lotharry, mouseX, mouseY, 200, 200);
+	image(theimages[whichimage], mouseX, mouseY, 200, 200);
 }
 
 function erase(){
